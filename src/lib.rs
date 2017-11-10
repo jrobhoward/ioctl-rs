e310562405ab2ff019eefa90a752f82b33ec7df2
James Howard <jhoward@thinkpad.jrh.net>
2017-11-09 20:06:19 -0600

    asdf

--- src/lib.rs
+++ src/lib.rs
@@ -71,10 +71,80 @@ pub fn tiocmbic(fd: RawFd, bits: c_int) -> io::Result<()> {
     }
 }
 
-/// Get audit queue length
+/// Query the current number of records in auditpipe
+pub fn ap_get_len(fd: RawFd) -> io::Result<u32> {
+    let result: u32 = 0;
+    match unsafe { ioctl(fd, AUDITPIPE_GET_QLEN, &result) } {
+        0 => Ok((result)),
+        _ => Err(io::Error::last_os_error()),
+    }
+}
+
+/// Query the current max number of records that may be queued for reading in auditpipe
+pub fn ap_get_qlimit(fd: RawFd) -> io::Result<u32> {
+    let result: u32 = 0;
+    match unsafe { ioctl(fd, AUDITPIPE_GET_QLIMIT, &result) } {
+        0 => Ok((result)),
+        _ => Err(io::Error::last_os_error()),
+    }
+}
+
+/// Set the current max number of records that may be queued for reading in auditpipe
+pub fn ap_set_qlimit(fd: RawFd, new_limit: u32) -> io::Result<()> {
+    match unsafe { ioctl(fd, AUDITPIPE_SET_QLIMIT, &new_limit) } {
+        0 => Ok(()),
+        _ => Err(io::Error::last_os_error()),
+    }
+}
+
+/// Query the highest possible max number of records that may be queued for reading in auditpipe
 pub fn ap_get_qlimit_max(fd: RawFd) -> io::Result<u32> {
-    let result:u32 = 0;
+    let result: u32 = 0;
     match unsafe { ioctl(fd, AUDITPIPE_GET_QLIMIT_MAX, &result) } {
+        0 => Ok((result)),
+        _ => Err(io::Error::last_os_error()),
+    }
+}
+
+/// Query the lowest possible max number of records that may be queued for reading in auditpipe
+pub fn ap_get_qlimit_min(fd: RawFd) -> io::Result<u32> {
+    let result: u32 = 0;
+    match unsafe { ioctl(fd, AUDITPIPE_GET_QLIMIT_MIN, &result) } {
+        0 => Ok((result)),
+        _ => Err(io::Error::last_os_error()),
+    }
+}
+
+/// Flush all outstanding records in auditpipe
+pub fn ap_flush(fd: RawFd) -> io::Result<()> {
+    match unsafe { ioctl(fd, AUDITPIPE_FLUSH) } {
+        0 => Ok(()),
+        _ => Err(io::Error::last_os_error()),
+    }
+}
+
+/// Query the max size of an individual
+pub fn ap_get_maxauditdata_size(fd: RawFd) -> io::Result<u32> {
+    let result: u32 = 0;
+    match unsafe { ioctl(fd, AUDITPIPE_GET_MAXAUDITDATA, &result) } {
+        0 => Ok((result)),
+        _ => Err(io::Error::last_os_error()),
+    }
+}
+
+/// Get auditpipe drop count
+pub fn ap_get_inserts(fd: RawFd) -> io::Result<(u64)> {
+    let result: u64 = 0;
+    match unsafe { ioctl(fd, AUDITPIPE_GET_INSERTS, &result) } {
+        0 => Ok((result)),
+        _ => Err(io::Error::last_os_error()),
+    }
+}
+
+/// Get auditpipe truncate count
+pub fn ap_get_reads(fd: RawFd) -> io::Result<(u64)> {
+    let result: u64 = 0;
+    match unsafe { ioctl(fd, AUDITPIPE_GET_READS, &result) } {
         0 => Ok((result)),
         _ => Err(io::Error::last_os_error()),
     }
@@ -82,7 +152,7 @@ pub fn ap_get_qlimit_max(fd: RawFd) -> io::Result<u32> {
 
 /// Get auditpipe drop count
 pub fn ap_get_drops(fd: RawFd) -> io::Result<(u64)> {
-    let result:u64 = 0;
+    let result: u64 = 0;
     match unsafe { ioctl(fd, AUDITPIPE_GET_DROPS, &result) } {
         0 => Ok((result)),
         _ => Err(io::Error::last_os_error()),
@@ -91,7 +161,7 @@ pub fn ap_get_drops(fd: RawFd) -> io::Result<(u64)> {
 
 /// Get auditpipe truncate count
 pub fn ap_get_truncates(fd: RawFd) -> io::Result<(u64)> {
-    let result:u64 = 0;
+    let result: u64 = 0;
     match unsafe { ioctl(fd, AUDITPIPE_GET_TRUNCATES, &result) } {
         0 => Ok((result)),
         _ => Err(io::Error::last_os_error()),
